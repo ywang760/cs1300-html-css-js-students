5a30c59c37d8d5b9b1ec6c83d5f64215c7b99eb2
ywang760 <yutong_wang5@brown.edu>
2022-09-27 20:40:27 -0400

    all

--- app.js
+++ app.js
@@ -26,16 +26,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
+  const filteredArray = fruitsArray.filter((item) => {
+    return item.id > 5;
+  })
+  console.log(filteredArray);
+  
   // TODO: Create a new HTML element to display your data
+  const newElement = document.createElement('div');
 
-  // TODO: Append your new element to the page
-
+  for (let i = 0; i< filteredArray.length; i++) {
+    text = filteredArray[i].name + " has sugar of " + filteredArray[i].nutritions.sugar + "</br>";
+    newElement.innerHTML += text;
+  }
+  console.log(newElement);
+  gallery.append(newElement);
 }
+// TODO: Append your new element to the page
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
 const exampleAddElement = () => {
